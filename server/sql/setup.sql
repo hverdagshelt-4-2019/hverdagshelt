23a8b626d71b8dcbb8fb1154942f5afdb6d6f455
odderikf <odderikf@stud.ntnu.no>
2019-01-08 12:40:12 +0100

    Modified setup, added ticket comments and merged private_person into person.

--- server/sql/setup.sql
+++ server/sql/setup.sql
@@ -1,9 +1,10 @@
 DROP TABLE IF EXISTS happening;
 DROP TABLE IF EXISTS happening_category;
+DROP TABLE IF EXISTS ticket_comment;
 DROP TABLE IF EXISTS ticket;
 DROP TABLE IF EXISTS ticket_category;
 DROP TABLE IF EXISTS admin;
-DROP TABLE IF EXISTS private_person_CROSS_commune;
+DROP TABLE IF EXISTS person_CROSS_commune;
 DROP TABLE IF EXISTS private_person;
 DROP TABLE IF EXISTS public_worker;
 DROP TABLE IF EXISTS company;
@@ -28,18 +29,12 @@ CREATE TABLE admin(
   FOREIGN KEY (id) REFERENCES person(id) ON DELETE CASCADE
 );
 
-CREATE TABLE private_person(
-  id INT PRIMARY KEY,
-
-  FOREIGN KEY (id) REFERENCES person(id) ON DELETE CASCADE
-);
-
-CREATE TABLE private_person_CROSS_commune(
+CREATE TABLE person_CROSS_commune(
   person_id INT,
   commune_id INT,
 
   PRIMARY KEY (person_id, commune_id),
-  FOREIGN KEY (person_id) REFERENCES private_person(id) ON DELETE CASCADE,
+  FOREIGN KEY (person_id) REFERENCES person(id) ON DELETE CASCADE,
   FOREIGN KEY (commune_id) REFERENCES commune(id) ON DELETE CASCADE
 );
 
@@ -75,12 +70,24 @@ CREATE TABLE ticket(
   submitted_time DATETIME NOT NULL,
   finished_time DATETIME DEFAULT NULL,
   status ENUM('unprocessed', 'in progress', 'done', 'denied') DEFAULT 'unprocessed',
+  lat DOUBLE DEFAULT NULL,
+  lng DOUBLE DEFAULT NULL,
 
 
-  FOREIGN KEY (submitter_id) REFERENCES private_person(id),
+  FOREIGN KEY (submitter_id) REFERENCES person(id),
   FOREIGN KEY (responsible_commune_id) REFERENCES commune(id),
   FOREIGN KEY (responsible_company_id) REFERENCES company(id),
   FOREIGN KEY (category_id) REFERENCES ticket_category(id)
+);
+
+CREATE TABLE ticket_comment(
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  ticket_id INT NOT NULL,
+  description VARCHAR(256) NOT NULL,
+  submitter_id INT NOT NULL,
+
+  FOREIGN KEY (submitter_id) REFERENCES person(id),
+  FOREIGN KEY (ticket_id) REFERENCES ticket(id)
 );
 
 CREATE TABLE happening_category(
