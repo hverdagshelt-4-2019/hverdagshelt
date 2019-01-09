d50a6a632886638c4558fbd34762545a83e63694
Brage <brage.wichstrom@gmail.com>
2019-01-09 10:52:11 +0100

    Worked on authentication system

--- server/src/server.js
+++ server/src/server.js
@@ -60,13 +60,31 @@ app.get("/communes")
 Post-functions
  */
 
-app.get("/login", (req, res) => {
-    res.json({
-        message: 'Login'
+app.post("/login", (req, res) => {
+    const user = {
+        email : 'test',
+        id : 1
+    }
+    jwt.sign({user}, 'key',{expiresIn: '30m'}, (err, token) => {
+        res.json({
+            token
+        })
     });
 });
 
-app.post("/ticket");
+app.post("/ticket", verifyToken, (req,res) => {
+    jwt.verify(req.token, 'key', (err, authData) =>{
+        if(err){
+            res.sendStatus(403);
+        }else {
+            res.json({
+                message: 'Posted',
+                authData
+            });
+        }
+    });
+
+});
 
 app.post("/event");
 
@@ -105,5 +123,19 @@ app.delete("/ticket/:id")
 app.delete("/user/:id")
 
 app.delete("/event/:id")
+
+// Verify token
+function verifyToken(req, res, next) {
+    const bearerHeader = req.headers['authorization'];
+
+    if(typeof bearerHeader !== 'undefined') {
+        const bearer = bearerHeader.split(' ');
+        const bearerToken = bearer[1];
+        req.token = bearerToken;
+        next();
+    }else {
+        res.sendStatus(403);
+    }
+}
 
 let server = app.listen(3000);
