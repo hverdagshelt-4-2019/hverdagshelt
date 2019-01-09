e22210156b5e97e8377ffa1a2cf4df5f4c7fae32
Michael Mellemseter <michame@stud.ntnu.no>
2019-01-09 09:26:31 +0100

    added navbar

--- client/src/index.js
+++ client/src/index.js
@@ -2,18 +2,18 @@
 /* eslint eqeqeq: "off" */
 
 import * as React from 'react';
-import { Component,} from 'react-simplified';
+import { Component } from 'react-simplified';
 import { HashRouter, Route, NavLink } from 'react-router-dom';
 import ReactDOM from 'react-dom';
 import { Alert, Menu, Footer, Info } from './widgets';
-
+import { Navbar_person } from './Navbar_person';
 
 const root = document.getElementById('root');
 if (root)
   ReactDOM.render(
     <HashRouter>
       <div>
-        
+        <Navbar_person />
       </div>
     </HashRouter>,
     root
