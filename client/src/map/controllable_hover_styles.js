e2365885e38e3dec697159419e35d65c57df43c4
Marius Fridheim <mariufri@stud.ntnu.no>
2019-01-09 15:06:33 +0100

    Working map with tickets on

---
+++ client/src/map/controllable_hover_styles.js
@@ -0,0 +1,29 @@
+const K_SIZE = 20;
+
+const greatPlaceStyle = {
+  
+  
+  position: 'absolute',
+  width: K_SIZE,
+  height: K_SIZE,
+  left: -K_SIZE / 2,
+  top: -K_SIZE / 2,
+
+  border: '5px solid #f44336',
+  borderRadius: K_SIZE,
+  backgroundColor: 'white',
+  textAlign: 'center',
+  color: '#3f51b5',
+  fontSize: 16,
+  fontWeight: 'bold',
+  padding: 4,
+  cursor: 'pointer'
+};
+
+const greatPlaceStyleHover = {
+  ...greatPlaceStyle,
+  border: '5px solid #3f51b5',
+  color: '#f44336'
+};
+
+export {greatPlaceStyle, greatPlaceStyleHover, K_SIZE};
