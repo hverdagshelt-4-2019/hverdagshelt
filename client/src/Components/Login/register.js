98898535c92ba09c160d27289cf9964d213023f8
Håkon Dalen Hestnes <haakodh@stud.ntnu.no>
2019-01-10 08:45:16 +0100

    Fixed merge confict

--- client/src/Components/Login/register.js
+++ client/src/Components/Login/register.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 //@flow
 
 import {ReactDOM} from 'react-dom';
@@ -6,13 +5,7 @@ import * as React from 'react';
 import { Component } from 'react-simplified';
 import { HashRouter, Route, NavLink } from 'react-router-dom';
 import {registerService} from '../../services/RegisterService';
-=======
-import { ReactDOM } from 'react-dom';
-import * as React from 'react';
-import { Component } from 'react-simplified';
-import { HashRouter, Route, NavLink } from 'react-router-dom';
-import { personService } from '../../services/personservice'; //Must be created, does not exist atm.
->>>>>>> 7bf9917deb850f0dea1ca40acfef556c83236976
+
 
 //Need route to login site.
 export class Register extends Component {
@@ -22,7 +15,6 @@ export class Register extends Component {
   password2 = null;
   warning = null;
 
-<<<<<<< HEAD
     render(){
         return(
             <div className="mx-auto" style={{width: '400px'}} align="center">
@@ -57,64 +49,6 @@ export class Register extends Component {
         else{
             this.warning = "Passordene du skrev inn stemmer ikke overens.";
         }
-=======
-  render() {
-    return (
-      <div className="mx-auto" style={{ width: '400px' }} align="center">
-        <br />
-        <h2>Registrer deg</h2>
-        <br />
-        <form>
-          <div className="form-group">
-            <input
-              className="form-control"
-              placeholder="Fullt navn"
-              onChange={evt => {
-                this.fullname = evt.target.value;
-              }}
-            />
-            <input
-              className="form-control"
-              placeholder="Email"
-              onChange={evt => {
-                this.email = evt.target.value;
-              }}
-            />
-            <input
-              className="form-control"
-              placeholder="Passord"
-              onChange={evt => {
-                this.password1 = evt.target.value;
-              }}
-            />
-            <input
-              className="form-control"
-              placeholder="Gjenta passord"
-              onChange={evt => {
-                this.password2 = evt.target.value;
-              }}
-            />
-          </div>
-        </form>
-        <button className="btn btn-primary" onClick={this.register}>
-          Opprett
-        </button>
-        <br />
-        <br />
-        <label>{this.warning}</label>
-        <br />
-        <label>Har du allerede en bruker?</label> <NavLink to="">Logg inn her!</NavLink>
-      </div>
-    );
-  }
-
-  register() {
-    if (this.password1 == this.password2) {
-      console.log('Registrerer...');
-      personService.postNewUser(this.email, this.password1).catch(error => console.log('Noe gikk galt.'));
-    } else {
-      this.warning = 'Passordene du skrev inn stemmer ikke overens.';
->>>>>>> 7bf9917deb850f0dea1ca40acfef556c83236976
     }
-  }
 }
+
