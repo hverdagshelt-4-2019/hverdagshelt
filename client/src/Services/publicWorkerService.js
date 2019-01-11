b7cc3f240ae34fb3b826f6a2ee46ccf526eb401d
Brage <brage.wichstrom@gmail.com>
2019-01-11 13:01:43 +0100

    added service objects

---
+++ client/src/Services/publicWorkerService.js
@@ -0,0 +1,28 @@
+// @flow
+import axios from 'axios';
+axios.interceptors.response.use(response => response.data);
+let url = "http://localhost:3000";
+
+class PublicWorker {
+    email;
+    commune;
+}
+
+let config = {
+    headers: {
+        Authorization: "Bearer " + localStorage.getItem('authToken'),
+    }
+}
+
+class PublicWorkerService {
+    createPublicWorker(email, commune): Promise<Object>{
+        let publicWorker = new PublicWorker();
+        publicWorker.email = email;
+        publicWorker.commune = commune;
+        return axios.post(url + '/publicworker', config);
+    }
+}
+
+export let publicWorkerService = new PublicWorkerService;
+
+
