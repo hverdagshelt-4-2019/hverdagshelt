8e548b777eb67da4eeb484c3ac1352d40de6144b
Aleksander Johansen <aleksjoh@stud.ntnu.no>
2019-01-14 16:50:31 +0100

    Added functionality for giving the user feedback when attempting to create a new account.

--- client/src/Components/Login/register.js
+++ client/src/Components/Login/register.js
@@ -3,14 +3,14 @@ import * as React from 'react';
 import { Component } from 'react-simplified';
 import userService from '../../Services/userService';
 import {NavLink } from 'react-router-dom';
-import styles from "./style.css";
 
 //Need route to login site.
 export default class Register extends Component {
-  email = null;
-  password1 = null;
-  password2 = null;
-  warning = null;
+  email = "";
+  password1 = "";
+  password2 = "";
+  warning = "";
+  success = "";
 
     render(){
         return(
@@ -19,6 +19,7 @@ export default class Register extends Component {
                     <br/>
                     <h2>Registrer deg</h2>
                     <br/>
+                    <label className="text-success">{this.success}</label>
                     <form>
                         <div className="form-group">
                             <input className="form-control" placeholder="Email" onChange={(evt) => {this.email = evt.target.value}}></input>
@@ -39,12 +40,21 @@ export default class Register extends Component {
     }
 
     register(){
-        if(this.password1 == this.password2) {
-            if(!this.checkFields()) return;
-            console.log("Registrerer...");
+        this.success = null;
+        this.warning = null;
+        if(!this.checkFields()) return;
+        if(this.password1 === this.password2) {
             userService.createUser(this.email, this.password1)
-                .then(data => console.log(data))//OK
-                .catch(err => console.log(err));
+                .then(res => {
+                    if(res.status === 200) this.success = "Ny bruker er registrert!";
+                    else {
+                        this.warning = "Kunne ikke legge til bruker fordi grunner.";
+                    }
+                })//OK
+                .catch(err => {
+                    console.log(err)
+                    this.warning = "Intern server error. Ikke gi oss mindre enn perfekt data."
+                });
         }
         else{
             this.warning = "Passordene du skrev inn stemmer ikke overens.";
@@ -52,8 +62,16 @@ export default class Register extends Component {
     }
 
     checkFields(){
-        if(this.email === null || this.password1 === null || this.password2 === null) {
+        if(this.email.trim() === "" || this.password1.trim() === "" || this.password2.trim() === "") {
             this.warning = "Du må fylle ut alle feltene for å opprette en bruker";
+            return false;
+        }
+        else if(this.password1.length < 8 || this.password2.length < 8){
+            this.warning = "Passordet ditt er ikke langt nok";
+            return false;
+        }
+        else if(this.email.length > 254){
+            this.warning = "Emailen din er for lang.";
             return false;
         }
         return true;
