94ccc2cfb64c13b6eb6900dd5a1fd9157807e62f
Aleksander Johansen <aleksjoh@stud.ntnu.no>
2019-01-14 14:20:25 +0100

    Removed fullname box in register page and added passord fields.

--- client/src/Components/Login/register.js
+++ client/src/Components/Login/register.js
@@ -6,11 +6,11 @@ import userService from '../../services/userService';
 
 //Need route to login site.
 export class Register extends Component {
-  fullname = null;
-  email = null;
-  password1 = null;
-  password2 = null;
-  warning = null;
+    fullname = null;
+    email = null;
+    password1 = null;
+    password2 = null;
+    warning = null;
 
     render(){
         return(
@@ -21,10 +21,9 @@ export class Register extends Component {
                     <br/>
                     <form>
                         <div className="form-group">
-                            <input className="form-control" placeholder="Fullt navn" onChange={(evt)=> {this.fullname = evt.target.value}}></input>
                             <input className="form-control" placeholder="Email" onChange={(evt) => {this.email = evt.target.value}}></input>
-                            <input className="form-control" placeholder="Passord" onChange={(evt) => {this.password1 = evt.target.value}}></input>
-                            <input className="form-control" placeholder="Gjenta passord" onChange={(evt) => {this.password2 = evt.target.value}}></input>
+                            <input type="password" className="form-control" placeholder="Passord" onChange={(evt) => {this.password1 = evt.target.value}}></input>
+                            <input type="password" className="form-control" placeholder="Gjenta passord" onChange={(evt) => {this.password2 = evt.target.value}}></input>
                         </div>
                     </form>
                     <button className="btn btn-primary" onClick={this.register}>Opprett</button>
@@ -34,20 +33,29 @@ export class Register extends Component {
                     <br/>
                     <label>Har du allerede en bruker?</label>{' '}
                     <NavLink exact to={'/'}>Logg inn her!</NavLink>
-                </div> 
+                </div>
             </div>
         )
     }
 
     register(){
         if(this.password1 == this.password2) {
+            if(!this.checkFields()) return;
             console.log("Registrerer...");
             userService.createUser(this.email, this.password1)
                 .then(data => console.log(data))//OK
-            .catch(err => console.log(err));
+                .catch(err => console.log(err));
         }
         else{
             this.warning = "Passordene du skrev inn stemmer ikke overens.";
         }
     }
-  }
+
+    checkFields(){
+        if(this.email === null || this.password1 === null || this.password2 === null) {
+            this.warning = "Du må fylle ut alle feltene for å opprette en bruker";
+            return false;
+        }
+        return true;
+    }
+}
