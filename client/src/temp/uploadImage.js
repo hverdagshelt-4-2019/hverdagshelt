cb82b111d7ee980f616c20272b959680d127245a
Marius Fridheim <mariufri@stud.ntnu.no>
2019-01-15 11:18:46 +0100

    upload image

--- client/src/temp/uploadImage.js
+++ client/src/temp/uploadImage.js
@@ -1,18 +1,18 @@
 // @flow
 /* eslint eqeqeq: "off" */
 import * as React from 'react';
-import {Component,} from 'react-simplified';
-import {NavLink } from 'react-router-dom';
+import { Component,} from 'react-simplified';
+import { NavLink,} from 'react-router-dom';
 
 export default class uploadImage extends Component {
 
-    addTicketI(){
-        let url = "http://localhost:3000/ticketI/";
+    addImage(id: number){
+        let url = "http://localhost:3000/image/";
         console.log("postImage");
         let file = document.getElementById("InputFile").files[0];
         console.log(file);
         let formData = new FormData();
-        formData.append("overskrift", "test");
+        formData.append("id", id);
         formData.append("uploaded_image", file);
         fetch(url,
             {
@@ -40,7 +40,7 @@ export default class uploadImage extends Component {
                     <input type="file" className="form-control-file" id="InputFile"/>
                     <small id="fileHelp" className="form-text text-muted">Last opp bildet som skal være på visningen av saken</small>
                 </div>
-                <button type="submit" onClick={this.addTicketI} style={{width: "100%"}} className="btn btn-primary">Legg til</button>
+                <button type="submit" onClick={() => {this.addImage(1)}} style={{width: "100%"}} className="btn btn-primary">Legg til</button>
             </form>
             );
         }
--- server/src/server.js
+++ server/src/server.js
@@ -679,9 +679,9 @@ export function create_app(pool) {
 
     /* Upload image with the ticetkId for the ticket that the image
     is connected to. This is to upload Image*/ 
-    app.post("/ticketI", (req, res) => {
+    app.post("/image", (req, res) => {
         console.log("Got POST-request from client");
-        console.log(req.body.overskrift);//temp delete
+        console.log("id: " + req.body.id);//temp delete
         console.log(req.files);//temp delete
 
         if (!req.files) {
@@ -692,38 +692,38 @@ export function create_app(pool) {
         console.log("files where uploaded");
         let file = req.files.uploaded_image;
         let img_name = file.name;
-        fs.readdir(path.join(client_public,'images'), (err, files) => {
-            img_name = files.length + img_name;
-            console.log(img_name);
-            if (file.mimetype == "image/jpeg" || file.mimetype == "image/png") {
-                console.log("Correct type of image");
-                file.mv(path.join(client_public,'images', img_name), function (err) {
+        img_name = req.body.id + img_name;
+        console.log(img_name);
+        if (file.mimetype == "image/jpeg" || file.mimetype == "image/png") {
+            console.log("Correct type of image");
+            file.mv(path.join(client_public,'images', img_name), function (err) {
 
-                    if (err) {
-                        console.log("Something went wrong");
-                        return res.status(500).send(err);
-                    }
+                if (err) {
+                    console.log("Something went wrong");
+                    return res.status(500).send(err);
+                }
 
-                    /*
-                    Here you have to add how the path will be saved in database. Some example code under
-                    let val = [overskrift, innhold, kategori, viktighet, img_name];
-                    caseDao.createOne(val, (status, data) => {
-                        res.status(status);
-                        res.json(data);
-                    });*/
-                });
-            }else{
-                console.log("Wrong type if image");
-                return res.status(400).send();
-            }
-        });
+                /*
+                Here you have to add how the path will be saved in database. Some example code under
+                let val = [overskrift, innhold, kategori, viktighet, img_name];
+                caseDao.createOne(val, (status, data) => {
+                    res.status(status);
+                    res.json(data);
+                });*/
+            });
+        }else{
+            console.log("Wrong type if image");
+            return res.status(400).send();
+        }
     });
 
     /* Upload image with the ticetkId for the ticket that the image
     is connected to. This is to edit Image*/ 
-    app.put("/image/:ticketId", (req, res) => {
-        console.log("Got PUT-request from client");
-        const { ticketId } = req.params;
+    app.put("/image", (req, res) => {
+        console.log("Got POST-request from client");
+        console.log("id: " + req.body.id);//temp delete
+        console.log(req.files);//temp delete
+
         if (!req.files) {
             console.log("no files were uploaded");
             return res.status(400).send("No files were uploaded.");
@@ -732,27 +732,29 @@ export function create_app(pool) {
         console.log("files where uploaded");
         let file = req.files.uploaded_image;
         let img_name = file.name;
-        fs.readdir(path.join(client_public,'images'), (err, files) => {
-            img_name = files.length + img_name;
-            console.log(img_name);
+        img_name = req.body.id + img_name;
+        console.log(img_name);
+        if (file.mimetype == "image/jpeg" || file.mimetype == "image/png") {
+            console.log("Correct type of image");
+            file.mv(path.join(client_public,'images', img_name), function (err) {
 
-            if (file.mimetype == "image/jpeg" || file.mimetype == "image/png") {
-                console.log("Correct type of image");
-                file.mv(path.join(client_public,'images', img_name), function (err) {
+                if (err) {
+                    console.log("Something went wrong");
+                    return res.status(500).send(err);
+                }
 
-                    if (err) {
-                        return res.status(500).send(err);
-                    }
-                    /*
-                    Here you have to add how the path will be saved in database. Some example code under
-                    let val = [img_name, ticketId];
-                    ticketDao.updateImage(val, (status, data) => {
-                        res.status(status);
-                        res.json(data);
-                    });*/
-                });
-            }
-        });
+                /*
+                Here you have to add how the path will be saved in database. Some example code under
+                let val = [overskrift, innhold, kategori, viktighet, img_name];
+                caseDao.createOne(val, (status, data) => {
+                    res.status(status);
+                    res.json(data);
+                });*/
+            });
+        }else{
+            console.log("Wrong type if image");
+            return res.status(400).send();
+        }
     });
 
     //get image from server side and send to frontend
