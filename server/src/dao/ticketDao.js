f2e7603edf7f3662e3b6fa64570efb5546d01419
Sindre Thomassen <sindrtho@hotmail.com>
2019-01-15 16:57:51 +0100

    Typo

--- server/src/dao/ticketDao.js
+++ server/src/dao/ticketDao.js
@@ -65,9 +65,9 @@ export default class TicketDao extends Dao {
     }
 
     getAllTickets(callback) {
-        super.query("SELECT t.id, email as submitter_email, responsible_commune, c2.name as company_name, category, ' +\n" +
-            "        'title, description, picture, submitted_time, finished_time, status, lat, lng FROM ticket t JOIN person p ' +\n" +
-            "        'ON p.id = t.submitter_id LEFT JOIN company c2 ON responsible_company_id = c2.id;", [], callback);
+        super.query("SELECT t.id, email as submitter_email, responsible_commune, c2.name as company_name, category, " +
+            "title, description, picture, submitted_time, finished_time, status, lat, lng FROM ticket t JOIN person p " +
+            "ON p.id = t.submitter_id LEFT JOIN company c2 ON responsible_company_id = c2.id;", [], callback);
     }
 }
 
