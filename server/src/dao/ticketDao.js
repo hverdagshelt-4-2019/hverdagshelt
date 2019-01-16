74f906a0b9a6bbe9dbb0209ee05847ead4e2bd68
odderikf <odderikf@stud.ntnu.no>
2019-01-16 13:53:44 +0100

    Indentation fix

--- server/src/dao/ticketDao.js
+++ server/src/dao/ticketDao.js
@@ -9,15 +9,15 @@ export default class TicketDao extends Dao {
             callback)
     }
 
-  getTicketsByCommune(communes, callback) {
-    super.query(
-      'SELECT t.id, email as submitter_email, responsible_commune, c2.name as company_name, category, ' +
-        'title, description, picture, submitted_time, finished_time, status, lat, lng FROM ticket t JOIN person p ' +
-        'ON p.id = t.submitter_id LEFT JOIN company c2 ON responsible_company_id = c2.id WHERE responsible_commune in(?);',
-      [communes],
-      callback
-    );
-  }
+    getTicketsByCommune(communes, callback) {
+        super.query(
+            'SELECT t.id, email as submitter_email, responsible_commune, c2.name as company_name, category, ' +
+            'title, description, picture, submitted_time, finished_time, status, lat, lng FROM ticket t JOIN person p ' +
+            'ON p.id = t.submitter_id LEFT JOIN company c2 ON responsible_company_id = c2.id WHERE responsible_commune in(?);',
+            [communes],
+            callback
+        );
+    }
 
     getTicketsByCompany(companyId, callback) {
         super.query(
@@ -28,7 +28,7 @@ export default class TicketDao extends Dao {
             callback
         )
     }
-  
+
     getTicketsByCategory(communes, categories, callback) {
         super.query("SELECT t.id, email as submitter_email, responsible_commune, c2.name as company_name, category, " +
             "title, description, picture, submitted_time, finished_time, status, lat, lng FROM ticket t JOIN person p " +
@@ -37,14 +37,14 @@ export default class TicketDao extends Dao {
             [communes, categories],
             callback)
     }
-    
+
     addTicket(json, callback) {
         let params = [json.userid, json.commune, json.category, json.title, json.description, json.picture, json.lat, json.long]
         super.query("INSERT INTO ticket (submitter_id, responsible_commune, category, title, description, picture, lat, lng) VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
             params,
             callback)
     }
-    
+
     editTicket(id, json, callback) {
         let params = [json.category, json.title, json.description, json.lat, json.long, id]
         super.query(
@@ -67,7 +67,7 @@ export default class TicketDao extends Dao {
         let params = [json.company, id];
         super.query("UPDATE ticket SET responsible_company_id = ? WHERE ticket.id = ?", params, callback);
     }
-    
+
     deleteTicket(id, callback) {
         super.query("DELETE FROM ticket WHERE id = ?",
             [id],
