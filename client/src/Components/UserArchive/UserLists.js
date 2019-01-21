ef896c3692069e0fca88f6cf71a21c47c86cffa4
Sindre Thomassen <sindrtho@hotmail.com>
2019-01-21 16:35:33 +0100

    Added functionality for listing all users, companies, admins and public workers

--- client/src/Components/UserArchive/UserLists.js
+++ client/src/Components/UserArchive/UserLists.js
@@ -1,61 +1,48 @@
-import userService from '../../Services/userService';
-import CompanyService from '../../Services/companyService';
-import PublicWorkerService from '../../Services/publicWorkerService';
-import AdminService from '../../Services/adminService';
+import * as React from 'react';
+import { Component } from 'react-simplified';
+import userService from "../../Services/userService";
 
-export class UserList extends Component {
-    users = [];
+
+export class UserList extends Component<{props: {people: Object[], title: string, callback: function, tab: string}}> {
 
     render() {
         return(
-            <div></div>
+            <div>
+                <h1>
+                    {
+                        this.props.title
+                    }
+                </h1>
+                {
+                    this.props.people.map(e => {
+                        return (
+                            <div className="card" style={{minWidth: "650px", width: "75%", margin: "auto"}}>
+                                <div className="card-body">
+                                    <div style={{display: "flex", alignItems: "center", flexDirection: "row", justifyContent: "space-between"}}>
+                                        <div>
+                                            <h5 className="card-title">
+                                                E-post: {e.email}
+                                            </h5>
+                                            <h5>
+                                                Bruker ID: {e.id}
+                                            </h5>
+                                            {
+                                                this.props.tab === 'c' &&
+                                                (<h5>
+                                                    Bedriftsnavn: {e.name}
+                                                </h5>)
+                                            }
+                                        </div>
+                                        <div className="p-5">
+                                            <button className="btn btn-danger" onClick={be => this.props.callback(e.email)}><h3>SLETT</h3></button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        )
+                    })
+                }
+            </div>
         );
     }
-
-    mounted() {
-        userService.getUsers()
-            .then(res => this.users = res.data)
-            .catch(err => console.log(err))
-    }
-}
-
-export class CompanyList extends Component {
-    companies = [];
-
-    render() {
-        return (
-            <div></div>
-        );
-    }
-
-    mounted() {
-        CompanyService.getCompanies()
-            .then(res => this.companies = res.data)
-            .catch(err => console.log(err))
-    }
-}
-
-export class AdminList extends Component {
-    admins = [];
-
-    render() {
-        return (
-            <div></div>
-        );
-    }
-
-    mounted() {
-        AdminService
-    }
-}
-
-export class PublicworkerList extends Component {
-    publicworkers = [];
-
-    render() {
-        return (
-            <div></div>
-        );
-    }
-
 }
