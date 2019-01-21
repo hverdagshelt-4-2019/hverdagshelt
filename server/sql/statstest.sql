c9f0bd9c878e428a4c816201f6ca114420cdfae9
Sindre Thomassen <sindrtho@hotmail.com>
2019-01-21 08:39:18 +0100

    ???

--- server/sql/statstest.sql
+++ server/sql/statstest.sql
@@ -9,10 +9,7 @@ select responsible_commune as 'commune', count(*) from ticket group by responsib
 
 select status, responsible_commune as 'commune', count(*) as 'number_of_tickets' from ticket group by commune, status;
 
-select category, responsible_commune as 'commune', count(*) as 'tickets_in_communes' from ticket group by resp-- National stats
-select status, count(*) as 'number_of_tickets_with_status' from ticket group by status;
-
-select category, count(*) as 'tickets_in_categories' from ticket group by category;
+select category, responsible_commune as 'commune', count(*) as 'tickets_in_communes' from ticket group by category, responsible_commune order by commune; -- National stats
 
 
 -- Communal stats
@@ -39,7 +36,7 @@ select a.responsible_commune, total, unprocessed, finished, `fuck sql` from  (se
 select a.category, total, unprocessed, finished, `fuck sql` from  (select category, count(*) as total from ticket GROUP BY category)  a left join
   (select category, count(*) as unprocessed from ticket where status != 'Fullført' group by category) c on a.category = c.category left join
   (SELECT category, count(*) as finished from ticket where status = 'Fullført' GROUP BY category) b on a.category = b.category left join
-  (select category, (select count(*) from ticket WHERE status = 'Fullført')/(select count(*) from ticket)*100 as 'fuck sql' from ticket group by category) d on a.category = d.category;onsible_commune, category;
+  (select category, (select count(*) from ticket WHERE status = 'Fullført')/(select count(*) from ticket)*100 as 'fuck sql' from ticket group by category) d on a.category = d.category;
 
 
 -- Percentage of solved (Total)
