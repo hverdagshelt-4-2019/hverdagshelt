e8a9e6d8b560a00d46ae693e3dd261fa2b031fad
Magomed Khatujevitsj Bakhmadov <magomedb@stud.ntnu.no>
2019-01-23 11:04:10 +0100

    Changed event.js

--- client/src/Components/Event/Event.js
+++ client/src/Components/Event/Event.js
@@ -11,121 +11,42 @@ import eventService from '../../Services/eventService';
 import { Alert } from '../../widgets';
 import { K_SIZE } from './../../map/controllable_hover_styles.js';
 
-@controllable(['center', 'zoom', 'hoverKey', 'clickKey'])
-export default class event extends Component<{ match: { params: { id: number } } }> {
-    event: {lat:any, lng:any, picture:any} = {lat: '', lng: '', picture: ''};
+export default class Event extends Component<{ match: { params: { id: number } } }> {
+    event: {picture:any} = {picture: ''};
     sub_date = null;
 
 
-
-    /*static propTypes = {
-        zoom: PropTypes.number, // @controllable
-        hoverKey: PropTypes.string, // @controllable
-        clickKey: PropTypes.string, // @controllable
-        onCenterChange: PropTypes.func, // @controllable generated fn
-        onZoomChange: PropTypes.func, // @controllable generated fn
-        onHoverKeyChange: PropTypes.func, // @controllable generated fn
-
-        greatPlaces: PropTypes.array
-    };
-    static defaultProps = {
-        center: {
-            lat: 63.42,
-            lng: 10.38
-        },
-        zoom: 13,
-        greatPlaces: [{ id: 'Temp ex', lat: 63.42, lng: 10.38 }]
-    };
-
-    shouldComponentUpdate = shouldPureComponentUpdate;
-
-    constructor(props) {
-        super(props);
-    }
-
-    _onChange = (center, zoom ) => {
-        this.props.onCenterChange(center);
-        this.props.onZoomChange(zoom);
-    };
-
-    _onChildClick = (key, childProps) => {
-        this.props.onCenterChange([childProps.lat, childProps.lng]);
-    };
-
-    _onChildMouseEnter = (key) => {
-        this.props.onHoverKeyChange(key);
-    };
-
-    _onChildMouseLeave = () => {
-        this.props.onHoverKeyChange(null);
-    };*/
-
     render() {
         if(!this.event) return (<></>);
-
-
         return (
-            <div>
-                <div className="container">
-                    <div className="row">
-                        <div className="col-lg-8">
-                            <h1>{this.event.title}</h1>
-
-                            <p className="col-lg-8">
-                                Registrert av: {this.event.submitter_id}
-                            </p>
-
-                            <hr />
-
-                            <p>
-                                <b>Dato:</b> {this.event.happening_time !== undefined && this.event.happening_time.replace('T', ' ').replace('.000Z', '')}
-                            </p>
-
-                            <p>
-                                <b>Kommune:</b> {this.event.commune_name}
-                            </p>
-
-                            <p>
-                                <b>Kategori:</b> {this.event.category}
-                            </p>
-
-                            <hr />
-
-                            <img id="picture" src="logo.png" className={"img-fluid "} alt="Responsive image"/>
-
-                            <hr />
-
-                            <p>{this.event.description}</p>
-
-
-
-                            <div style={{ height: '100px' }} />
-                        </div>
-                    </div>
+             <div className="aroundStuff">
+                <div className="card">
+                <img id="picture" className="card-img-top" style={{maxWidth: "100%", maxHeight: "300px"}} alt="Responsive image"/>
+                <div className="card-header">
+                <h1>{this.event.title}</h1>
                 </div>
-            </div>
+                <ul className="list-group list-group-flush">
+                    <li className="list-group-item"><b>Dato:</b> {this.event.happening_time !== undefined && this.event.happening_time.replace('T', ' ').replace('.000Z', '').slice(0, -3)}</li>
+                    <li className="list-group-item"><b>Kommune:</b> {this.event.commune_name}</li>
+                    <li className="list-group-item"><b>Kategori:</b> {this.event.category}</li>
+                    <li className="list-group-item"><b>Beskrivelse:</b> {this.event.description}</li>
+                </ul>
+                </div>
+           </div>
         );
     }
 
     mounted() {
-        console.log('mounting');
         eventService.
             getEvent(this.props.match.params.id)
             .then(event => {
                 this.event = event.data[0];
-                this.sub_date =
-                    this.event.happening_time.split('T', 1)[0] + ' ' + this.event.happening_time.split('T')[1].split('.', 1);
+                this.sub_date = this.event.happening_time.split('T', 1)[0] + ' ' + this.event.happening_time.split('T')[1].split('.', 1);
                 console.log(this.props.match.params.id);
-               /* this.props.greatPlaces[0].lat=this.event.lat;
-                this.props.greatPlaces[0].lng=this.event.lng;
-                console.log("lat: " + this.event.lat + ". lng: " + this.event.lng)
                 console.log(this.event.picture);
-                this.getImage(this.event.picture); //xss
-                this.state.center.lat=this.event.lat;
-                this.state.center.lng=this.event.lng;
-                window.location.reload();*/
+                this.getImage(this.event.picture);
             })
-            .catch((error: Error) => Alert.danger(error.message));
+            .catch((error: Error) => console.log(error.message));
     }
 
 
@@ -135,12 +56,4 @@ export default class event extends Component<{ match: { params: { id: number } }
         if(picture)
             picture.setAttribute('src', imageLink);
     }
-
-    componentDidMount() {
-        console.log('did mount');
-        {
-            this.getImage(this.event.picture);
-        }
-    }
-
 }
